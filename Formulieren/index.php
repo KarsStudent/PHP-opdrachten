8840c268ec66478aee1ee5dfb9d7e53b71706907
eJwtzE2PgjAUheH/0jULBNHS7WSiCUt1Mrum9N5CQ6XNLeBX/O82ht3JSZ73xZTWGOPZDzgywQxiXu23YDjnCgGwrncbU/E8rwsoNkbrXc2hApaxQH6xgLTS11qS05rqei+r/6X5baDpjd4u56UbIPxcejwcy7b8uxdPHctTSn2JnB4Bk2tREVJ6o/bfQ8HVjiLMrbNaDvjICBUIT10awWdzRBJ4VdZlN0+Dcf6WMN6DJYxSTUyMs3Pv9wcAJlEW <97106725@st.deltion.nl>
2023-09-28 10:34:07 +0200

    First commit

---
+++ Formulieren/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP opdracht - vertaler</title>
+</head>
+<body>
+
+    <form method="post" action="">
+        Naam: <input type="text" name="naam" placeholder="Uw naam" required><br>
+
+        Land: 
+        <select name="land">
+            <option value="NL">Nederland</option>
+            <option value="DE">Duitsland</option>
+            <option value="EN">Engeland</option>
+        </select>
+        <br>
+        <input type="submit" name="submit" value="gegevens versturen">
+    </form>
+
+</body>
+</html>
