8ee9884fa10140acb39d22b9d3f7f3993e338361
eJwtzE2PgjAUheH/0jULBNHS7WSiCUt1Mrum9N5CQ6XNLeBX/O82ht3JSZ73xZTWGOPZDzgywQxiXu23YDjnCgGwrncbU/E8rwsoNkbrXc2hApaxQH6xgLTS11qS05rqei+r/6X5baDpjd4u56UbIPxcejwcy7b8uxdPHctTSn2JnB4Bk2tREVJ6o/bfQ8HVjiLMrbNaDvjICBUIT10awWdzRBJ4VdZlN0+Dcf6WMN6DJYxSTUyMs3Pv9wcAJlEW <97106725@st.deltion.nl>
2023-10-03 11:59:17 +0200

    Finished Formulieren

--- Formulieren/index.php
+++ Formulieren/index.php
@@ -10,15 +10,51 @@
     <form method="post" action="">
         Naam: <input type="text" name="naam" placeholder="Uw naam" required><br>
 
-        Land: 
-        <select name="land">
-            <option value="NL">Nederland</option>
-            <option value="DE">Duitsland</option>
-            <option value="EN">Engeland</option>
+        Land:
+        <select name="land" required>
+            <option value="">Maak uw keuze</option>
+            <option value="NL" <?php selected("NL") ?>>Nederland</option>
+            <option value="DE" <?php selected("DE") ?>>Duitsland</option>
+            <option value="EN" <?php selected("EN") ?>>Engeland</option>
+            <option value="FR" <?php selected("FR") ?>>Frankrijk</option>
+            <option value="ES" <?php selected("ES") ?>>Spanje</option>
+            <option value="IT" <?php selected("IT") ?>>Italië</option>
         </select>
         <br>
         <input type="submit" name="submit" value="gegevens versturen">
     </form>
+
+    <?php
+
+        if(isset($_POST["submit"])) {
+            $naam = $_POST["naam"];
+            $land = $_POST["land"];
+
+            if($land == "NL") {
+                echo "<p>Goedemorgen $naam</p>";
+            } elseif($land == "DE") {
+                echo "<p>Guten morgen $naam</p>";
+            } elseif ($land == "EN") {
+                echo "<p>Good morning $naam</p>";
+            } elseif ($land == "FR") {
+                echo "<p>Bonjour $naam</p>";
+            } elseif ($land == "ES") {
+                echo "<p>Buen día $naam</p>";
+            } elseif ($land == "IT") {
+                echo "<p>Buongiorno $naam</p>";
+            }
+        }
+
+        function selected($selected) {
+            if(isset($_POST["submit"])) {
+                $land = $_POST["land"];
+
+                if($selected == $land) {
+                    echo "selected";
+                }
+            }
+        }
+    ?>
 
 </body>
 </html>
